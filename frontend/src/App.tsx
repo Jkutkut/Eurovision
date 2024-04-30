375ead14bdfad4ada061557e6d941d705669d8cb
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-04-30 12:41:55 +0200

    setup front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,17 @@ import Login from './pages/Login';
 import MainPage from './pages/MainPage';
 
 const App = () => {
-  let login = localStorage.getItem('login');
-  if (login == null) {
-    return (
-      <Login />
-    );
-  }
+  // let login = localStorage.getItem('login');
+  // if (login == null) {
+  //   return (
+  //     <Login />
+  //   );
+  // }
 
-  return (
-    <MainPage user={login} />
-  )
+  // return (
+  //   <MainPage user={login} />
+  // )
+  return <></>;
 }
 
 export default App;
