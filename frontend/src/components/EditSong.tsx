38ea1271a966c6b77ce509d6b8639f84e378f3db
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-05-11 12:22:31 +0200

    refactor front: confirmodal in editsong

--- frontend/src/components/EditSong.tsx
+++ frontend/src/components/EditSong.tsx
@@ -3,8 +3,7 @@ import useForm from '../hooks/useForm';
 import UserScore from '../models/UserScore';
 import Song from '../models/Song';
 
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import ConfirmModal from './ConfirmModal';
 
 interface Props {
   song: Song;
@@ -30,47 +29,34 @@ const EditSong = ({song: songObj, userScore, saveCallback, cancelCallback}: Prop
     });
   };
   return (
-    <Modal
-      show={true}
-      onHide={cancelCallback}
-      keyboard={true}
+    <ConfirmModal
+      title={country}
+      cancelCallback={cancelCallback}
+      confirmCallback={save}
     >
-      <Modal.Header closeButton>
-        <Modal.Title>{country}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="row">
-          <div className="col">
-            Artist: <i>{artist}</i>
-          </div>
-          <div className="col">
-            Song: <i>{song}</i>
-          </div>
+      <div className="row">
+        <div className="col">
+          Artist: <i>{artist}</i>
         </div>
+        <div className="col">
+          Song: <i>{song}</i>
+        </div>
+      </div>
 
-        <br />
-        <div className="form-group">
-          <label htmlFor="nickname">Nickname</label>
-          <input type="text" className="form-control" placeholder="The name they'll be remembered by"
-            name="nickname" aria-describedby="nicknameHelp"
-            value={nickname} onChange={(e) => onChange(e)} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="notes">Notes</label>
-          <textarea className="form-control" name="notes" rows={3}
-            placeholder="Anything you want to remember about this song"
-            value={notes} onChange={(e) => onChange(e)} />
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={cancelCallback}>
-          Cancel
-        </Button>
-        <Button variant="primary" onClick={save}>
-          Save
-        </Button>
-      </Modal.Footer>
-    </Modal>
+      <br />
+      <div className="form-group">
+        <label htmlFor="nickname">Nickname</label>
+        <input type="text" className="form-control" placeholder="The name they'll be remembered by"
+          name="nickname" aria-describedby="nicknameHelp"
+          value={nickname} onChange={(e) => onChange(e)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="notes">Notes</label>
+        <textarea className="form-control" name="notes" rows={3}
+          placeholder="Anything you want to remember about this song"
+          value={notes} onChange={(e) => onChange(e)} />
+      </div>
+    </ConfirmModal>
   );
 };
 
